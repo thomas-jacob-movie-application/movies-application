f385a078334967f48d52ee207dcdeca92aaaa938
Jacob Barrow <jacob.barrow@gmail.com>
2019-11-19 06:59:18 -0600

    working functionality for delete, add, and edit buttons

--- src/index.js
+++ src/index.js
@@ -32,39 +32,39 @@ $(document).ready(() => {
                     <div class="card col-md-3 p-1" id="${id}">
                        <img src=${image} alt="yes">
                        <h1>${title}</h1>
-                       <h3>Rating: ${rating} stars</h3>
+                       <h3>${rating} stars</h3>
                        <i class="far fa-trash-alt trash"></i> 
                        <i class="fas fa-edit edit"></i>       
                        <h6>${id}</h6>   
                   </div>
                    `);
                 });
-                $(newRating).on('click', function () {
-                    let newId = $(this).parent().children().first().next().next().next().next().next().html().substr(1,1);
-                    let newTitle = $(this).parent().children().first().next().html().substr(0, 15);
-                    let newRating = $(this).parent().children().first().next().next().html().substr(8, 1);
 
-                    console.log(newRating);
-                    $('#editText').val(newTitle);
-                    $('#editRating').val(newRating);
+                $('.edit').on('click', function () {
+                    let newId = $(this).parent().children().first().next().next().next().next().next().html().substr(0,1);
+                    console.log(newId);
+                    let oldTitle = $(this).parent().children().first().next().html().substr(0, 15);
+                    let oldRating = $(this).parent().children().first().next().next().html().substr(0,1 );
+                    $('#editText').val(oldTitle);
+                    $('#editRating').val(oldRating);
                     $('.popUp').removeClass('invisible');
                     $('.editButton').on('click',function() {
-                        newTitle = $('#editText').val();
-                        newRating = $('#editRating').val();
-                            patchMovie({
-                                "title": newTitle,
-                                "rating": newRating
-                            },    newId)
+                        $('.popUp').addClass('invisible');
+                        let newTitle = $('#editText').val();
+                        let newRating = $('#editRating').val();
+                        patchMovie({
+                            "title": newTitle,
+                            "rating": newRating
+                        }, newId);
                         $('#movieHolder').html("");
-                        $('#preloader').removeClass("invisible");
+                        $('#preloader').removeClass('invisible');
                         getMovies()
                             .then((movies) => {
                                 makeMoviesAppear(movies)
+                                $('#preloader').addClass('invisible');
                             });
-                        $('#preloader').addClass("invisible");
-
                     })
-                    });
+                });
 
                 $('.trash').click(function () {
                     let idVariable = $(this).parent().attr('id');
@@ -73,9 +73,10 @@ $(document).ready(() => {
                     $('#preloader').removeClass("invisible");
                     getMovies()
                         .then((movies) => {
-                            makeMoviesAppear(movies)
+                            makeMoviesAppear(movies);
+                            $('#preloader').addClass("invisible");
+
                         });
-                    $('#preloader').addClass("invisible");
 
                 })
                 };
@@ -90,8 +91,8 @@ $(document).ready(() => {
                     "title": $('#addText').val(),
                     "rating": $('#addSelect').val(),
                 });
-                // $('#movieHolder').html("");
-                // $('#preloader').removeClass('invisible');
+                $('#movieHolder').html("");
+                $('#preloader').removeClass('invisible');
                 getMovies()
                     .then((movies) => {
                         makeMoviesAppear(movies);
@@ -133,33 +134,22 @@ $(document).ready(() => {
 
         })
 
-    // .then((movies) => {
-    // console.log('Here are all the movies: ');
-    // movies.forEach(({title, rating}) => {
-    //     console.log(`title:${title} - rating: ${rating}`);
-    // });
-    // }).catch((error) => {
-    //     alert('Oh no! Something went wrong.\nCheck the console for details.');
-    //     console.log(error);
-    // });
+// THINGS THAT MAY BE BROKEN
+//     $('.editButton').on('click',function() {
+//         newTitle = $('#editText').val();
+//         newRating = $('#editRating').val();
+//         patchMovie({
+//             "title": newTitle,
+//             "rating": newRating
+//         },    newId);
+//         $('#movieHolder').html("");
+//         $('#preloader').removeClass("invisible");
+//         getMovies()
+//             .then((movies) => {
+//                 makeMoviesAppear(movies);
+//             });
+//         $('#preloader').addClass("invisible");
 
-//************************
-//      EDIT MOVIE DATA
-//************************
-
-// .then(getMovies).then((movies) => {
-//   console.log('Here are all the books:');
-//   movies.forEach(({title, rating}) => {
-//     console.log(`${title}, ${rating}`);
-//   });
-// }).catch((error) => {
-//   alert('Oh no! Something went wrong.\nCheck the console for details.');
-//   console.log(error);
-// });
-
-//************************
-//      DELETE MOVIE
-//************************
-
+    // })
 
 })
