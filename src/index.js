81bea175128ebff56a29d64e6d405553d0583833
Jacob Barrow <jacob.barrow@gmail.com>
2019-11-18 18:39:36 -0600

    added prepopulate edit cards function

--- src/index.js
+++ src/index.js
@@ -22,85 +22,101 @@ $(document).ready(() => {
 
 
     //TEST OUT FUNCTION ONCE YOU HAVE ACTUAL MOVIES IN DB
-
+    let i = 0;
     getMovies()
         .then((movies) => {
-            console.log('Here are all the movies:');
-            $('#preloader').html("");
             let makeMoviesAppear = (movies) => {
+                $('#preloader').addClass("invisible");
                 movies.forEach(({title, rating, id, image}) => {
                     $('#movieHolder').append(`
-                <div class="card col-md-3 p-1" id="${id}">
-                   <img src=${image} alt="yes">
-                   <h1>${title}</h1>
-                   <h3>Rating: ${rating} stars</h3>
-                   <i class="far fa-trash-alt trash"></i>           
-              </div>
-            `);
-                    console.log(`id#${id} - ${title} - rating: ${rating}`);
-                })
-            }
+                    <div class="card col-md-3 p-1" id="${id}">
+                       <img src=${image} alt="yes">
+                       <h1>${title}</h1>
+                       <h3>Rating: ${rating} stars</h3>
+                       <i class="far fa-trash-alt trash"></i> 
+                       <i class="fas fa-edit edit"></i>          
+                  </div>
+                `);
+                });
+
+                $('.edit').on('click', function () {
+                    let newTitle = $(this).parent().children().first().next().html().substr(0, 200);
+                    console.log(newTitle);
+                    let newRating = $(this).parent().children().first().next().next().html().substr(7, 3);
+                    console.log(newRating);
+                    $('.popUp').removeClass('invisible');
+                    $('#editText').val(newTitle);
+                    $('#editRating').val(newRating);
+                });
+                $('.trash').click(function () {
+                    let idVariable = $(this).parent().attr('id');
+                    deleteMovie(idVariable);
+                    getMovies()
+                        .then((movies) => {
+                            makeMoviesAppear(movies)
+                        });
+                    })
+                };
             makeMoviesAppear(movies);
 
-//************************
-//      ADD NEW MOVIE
-//************************
+
+            //************************
+            //      ADD NEW MOVIE
+            //************************
             $('#addButton').click(() => {
                 let newMovie = postMovie({
                     "title": $('#addText').val(),
                     "rating": $('#addSelect').val(),
-                })
+                });
                 $('#movieHolder').html("");
-
+                $('#preloader').removeClass('invisible');
                 getMovies()
                     .then((movies) => {
                         makeMoviesAppear(movies);
+                        $('#preloader').addClass('invisible');
                     });
 
-
-                $('.trash').click(function () {
-                    let idVariable = $(this).parent().attr('id');
-                    deleteMovie(idVariable);
-                    // makeMoviesAppear();
-                });
+            });
 
 
-//************************
-//      SPECIFY ONE MOVIE
-//************************
+            //************************
+            //      SPECIFY ONE MOVIE
+            //************************
 
 
-//FIND A WAY TO USE GETMOVIE
-                $('#searchButton').on('click', (e) => {
-                    e.preventDefault();
-                    getMovies()
-                        .then((movies) => {
-                            movies.forEach(({title, rating, id, image}) => {
-                                if (title.toLowerCase().includes($('#searchText').val().toLowerCase())) {
-                                    $('#movieHolder').html(`
-                <div class="card col-md-3">
-                   <img src=${image} alt="yes">
-                   <h1>${title}</h1>
-                   <h3>Rating: ${rating} stars</h3>
-                     <i class="far fa-trash-alt trash"></i>
-                </div>
-                `)
-                                }
-                            })
+            //FIND A WAY TO USE GETMOVIE
+            $('#searchButton').on('click', (e) => {
+                e.preventDefault();
+                getMovies()
+                    .then((movies) => {
+                        movies.forEach(({title, rating, id, image}) => {
+                            if (title.toLowerCase().includes($('#searchText').val().toLowerCase())) {
+                                $('#movieHolder').html(`
+                                        <div class="card col-md-3">
+                                           <img src=${image} alt="yes">
+                                           <h1>${title}</h1>
+                                           <div id="popUp"></div>
+                                           <h3>Rating: ${rating} stars</h3>
+                                             <i class="far fa-trash-alt trash"></i>
+                                             <i class="fas fa-edit edit"></i>
+                                        </div>
+                                        `)
+                            }
                         })
-                });
+                    })
+            });
 
+        })
 
-                // .then((movies) => {
-                // console.log('Here are all the movies: ');
-                // movies.forEach(({title, rating}) => {
-                //     console.log(`title:${title} - rating: ${rating}`);
-                // });
-                // }).catch((error) => {
-                //     alert('Oh no! Something went wrong.\nCheck the console for details.');
-                //     console.log(error);
-                // });
-            })
+    // .then((movies) => {
+    // console.log('Here are all the movies: ');
+    // movies.forEach(({title, rating}) => {
+    //     console.log(`title:${title} - rating: ${rating}`);
+    // });
+    // }).catch((error) => {
+    //     alert('Oh no! Something went wrong.\nCheck the console for details.');
+    //     console.log(error);
+    // });
 
 //************************
 //      EDIT MOVIE DATA
@@ -122,9 +138,6 @@ $(document).ready(() => {
 //************************
 //      DELETE MOVIE
 //************************
-
-
-        })
 
 
 })
